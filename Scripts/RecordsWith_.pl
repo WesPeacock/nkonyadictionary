3f34527956b3284868622a5ac08d4a6450511ccd
Wes Peacock <wes_peacock@sil.org>
2012-11-27 06:39:17 -0700

    Save various script files

---
+++ Scripts/RecordsWith_.pl
@@ -0,0 +1,30 @@
+# usage perl -f Scripts\RecordsWithBlanks.pl <"Nkolex in Unicode.txt" >tmp\x.tmp
+# and then load tmp\x.tmp into Toolbox to process the records with blanks
+
+use strict;
+use warnings;
+use English;
+
+my $recmark="\\lx ";
+$RS = $recmark;
+
+RECORD: while (<>) {
+	if ($NR== 1)  {  # save header info
+		my $fileheader=$_;
+		next RECORD
+		};
+	my $count=0;
+
+	# move record marker from the end of the record to the beginning
+	chomp;
+	my $recstring = $recmark. $_;
+	$_=$recstring;
+
+	# Processing for each record goes after this
+
+	if ($recstring =~ /\\np /) {
+		# any record with a phonology note
+		print;
+		$count++;
+		};
+}
