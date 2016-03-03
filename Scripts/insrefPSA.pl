e40c9971ddfb12051631927af109b35fbefd7743
Wes Peacock <wes_peacock@sil.org>
2016-03-03 15:38:42 -0700

    Make a version of insref for Psalms
    * Improves the documentation at the top of the script.

--- Scripts/insrefPSA.pl
+++ Scripts/insrefPSA.pl
@@ -1,19 +1,20 @@
-#usage: perl insref.pl <input >output
+#usage: perl insrefPSA.pl <input >output
 # inserts a \ref marker before each \v in a Scriputure SFM file
 # \rev ID:chapter:verse
 # On Linux system do:
 # fromdos input
 # on the SFM file before running this to delete carriage returns on the markers
-# 2 digit only cf insrefPSA.pl
+# Handles Psalms - 3 digits leading 0s
 
-$chapter=$verse="00";
+$chapter=$verse="000";
 $id="___";
-print "\\ref ___:00:00\n";
+print "\\ref ___:000:000\n";
 while (<>) {
 	if (/\\v /) {
 		@refmark=split / /;
 		$verse = $refmark[1];
 		chomp $verse;
+		if ($verse+0 < 100) {$verse ="0".$verse;};
 		if ($verse+0 < 10) {$verse ="0".$verse;};
 		print "\\ref $id:$chapter:$verse\n";
 		}
@@ -21,8 +22,9 @@ while (<>) {
 		@refmark=split / /;
 		$chapter = $refmark[1];
 		chomp $chapter;
+		if ($chapter+0 < 100) {$chapter ="0".$chapter;};
 		if ($chapter+0 < 10) {$chapter ="0".$chapter;};
-		$verse="00";
+		$verse="000";
 		print "\\ref $id:$chapter:$verse\n";
 		}
 	elsif (/\\id /) {
