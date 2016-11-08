dbd7031e074e0aa93941f632fc8281613a62c6ce
Wes Peacock <wes_peacock@sil.org>
2016-11-08 09:42:44 -0700

    Document whether fields have trailing ##
     * also gets rid of some of the extraneous \n

--- Scripts/se2lx.pl
+++ Scripts/se2lx.pl
@@ -14,12 +14,12 @@ use English;
 
 my $dt = sprintf ("\\dt %02d/%s/%s##", (localtime)[3], (substr localtime(), 4, 3), (localtime)[5]+1900);
 
-my $lxfield =""; # key of master record
-my $subentry = ""; # text of subentry record
-my $hmno = ""; # text of homograph number if any
-my $beforestuff = "";	#preceding subentry record i.e.: \lx ... <char before \se>
-my $afterstuff = "";	# text of master record following subentry record  i.e. <field following subentry>...\dt field 
-my $snno="";
+my $lxfield =""; # key of master record -- no ##
+my $subentry = ""; # text of subentry record -- trailing ##
+my $hmno = ""; # text of homograph number if any -- no ##
+my $beforestuff = "";	#preceding subentry record i.e.: \lx ... <char before \se> -- trailing ##
+my $afterstuff = "";	# master record text following subentry i.e. <field following subentry>...\dt field  -- trailing ##
+my $snno=""; # text of sense number if any -- no ##
 my @snfields = "";
 
 m/\\lx ([^#]*?)##/;  $lxfield=$1;
@@ -35,8 +35,7 @@ while (/\\se.*?(?=(\\sn|\\se|\\dt|\\ps[^#]*?##\\sn))/)  {
 
 	$subentry =~ s/\\se ([^#]*?)##/\\lx $1##\\mn $lxfield$hmno$snno##/;
 
-	print "$subentry$dt##\n" ;
-
+	print "$subentry$dt##" ;
 
 	# delete subentry from master record and change date
 	s/\\se.*?(?=(\\sn|\\se|\\dt|\\ps[^#]*?##\\sn))//;
