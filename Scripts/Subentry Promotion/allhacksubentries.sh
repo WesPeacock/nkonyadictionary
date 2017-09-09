e29ab5ca1cf8ef9d8a0b814d17a444ee61fe2b27
Wes Peacock <wes_peacock@sil.org>
2017-09-09 12:56:04 -0600

    More CRLF stuff

--- Scripts/Subentry Promotion/allhacksubentries.sh
+++ Scripts/Subentry Promotion/allhacksubentries.sh
@@ -7,12 +7,12 @@
 # in PromoteSubentries.ini.org -- change {in|out}filename=... to {in|out}filename=/some/path/... throughout
 # However, the PromoteSubentries.ini must always be in the current directory
 # Todo send STDERR of perl -f hackFWvariants.pl append to a log file -- in the meantime use screencapture
-awk '{ gsub(/hackFWvariants1/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini ; perl -f hackFWvariants.pl 
-read -n1 -r -p "Press space to continue..." key 
-awk '{ gsub(/hackFWvariants2/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini ; perl -f hackFWvariants.pl 
-read -n1 -r -p "Press space to continue..." key 
-awk '{ gsub(/hackFWvariants3/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini  ; perl -f hackFWvariants.pl 
-read -n1 -r -p "Press space to continue..." key 
-awk '{ gsub(/hackFWvariants4/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini  ; perl -f hackFWvariants.pl 
-read -n1 -r -p "Press space to continue..." key 
-awk '{ gsub(/hackFWvariants5/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini  ; perl -f hackFWvariants.pl 
+awk '{ gsub(/hackFWvariants1/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini ; perl -f hackFWvariants.pl ;#
+read -n1 -r -p "Press space to continue..." key ;#
+awk '{ gsub(/hackFWvariants2/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini ; perl -f hackFWvariants.pl ;#
+read -n1 -r -p "Press space to continue..." key ;#
+awk '{ gsub(/hackFWvariants3/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini  ; perl -f hackFWvariants.pl ;#
+read -n1 -r -p "Press space to continue..." key ;#
+awk '{ gsub(/hackFWvariants4/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini  ; perl -f hackFWvariants.pl ;#
+read -n1 -r -p "Press space to continue..." key ;#
+awk '{ gsub(/hackFWvariants5/, "hackFWvariants") } ; { print}' <PromoteSubentries.ini.org >PromoteSubentries.ini  ; perl -f hackFWvariants.pl ;#
