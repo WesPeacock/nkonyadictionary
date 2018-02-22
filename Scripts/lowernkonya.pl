1b34f76f88a9cb405ac1373c5ae2f735371c8533
Wes Peacock <wes_peacock@sil.org>
2018-02-22 11:12:43 -0700

    Add usage comments to lowernkonya.pl script

--- Scripts/lowernkonya.pl
+++ Scripts/lowernkonya.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/env perl
+# call with perl -p argument:
+#perl -pf lowernkonya.pl input filefield
+# or pipe like:
+#  echo "HƐlLo WƲrld Ŋow Ɩs the time" |perl -pf lowernkonya.pl
  use utf8;      # so literals and identifiers can be in UTF-8
  use v5.12;     # or later to get "unicode_strings" feature
  use strict;    # quote strings, declare variables
