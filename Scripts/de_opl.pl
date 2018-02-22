65e030c42a47c1bc8fd056b2901f5321ea13f2a5
Wes Peacock <wes_peacock@sil.org>
2018-02-22 10:00:12 -0700

    Add a script to undo opl.pl -- de_opl.pl

---
+++ Scripts/de_opl.pl
@@ -0,0 +1,7 @@
+# perl -pf de_opl.pl FileNameAfteropl.SFM
+# undoes opl.pl
+# Converts SFM file from one record per line to one field per line
+# occurences of '_hash_'  are changed to back to '#'
+chomp;
+s/#/\n/g;
+s/\_\_hash\_\_/#/g;
