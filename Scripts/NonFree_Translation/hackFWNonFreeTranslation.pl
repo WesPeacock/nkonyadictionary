49de9640a00f5b5e679af9fad24812b85afb9dc3
Wes Peacock <wes_peacock@sil.org>
2018-02-23 11:13:00 -0700

    Remove CRs from the config file under linux
     * creates a copy of the config file in /tmp under linux

--- Scripts/NonFree_Translation/hackFWNonFreeTranslation.pl
+++ Scripts/NonFree_Translation/hackFWNonFreeTranslation.pl
@@ -27,9 +27,14 @@ use Config::Tiny;
 # infilename=Nktest.fwdata
 # outfilename=Nktest.new.fwdata
 
-# Todo: Look up the NonFreeTranslationRTguid by searching for "Literal translation" and returning the rt's guid
-`dos2unix PromoteSubentries.ini` ;  # handle Windows CRLF nonsense 
-my $config = Config::Tiny->read( 'PromoteSubentries.ini' );
+my $configfile = 'PromoteSubentries.ini';
+# Windows CRLF nonsense
+if ( $^O =~ /linux/)  {
+	`dos2unix < $configfile  >/tmp/$configfile ` ;
+	$configfile = '/tmp/'.$configfile;
+	}
+my $config = Config::Tiny->read($configfile);
+
 #ToDo: get the pathname of the INI file from $0 so that the two go together
 die "Couldn't find the INI file\nQuitting" if !$config;
 my $starttag = $config->{hackFWNonFreeTranslation}->{NonFreeTranslationStarttag};
